7768a627316ff37639ad7363088888b7b66a34ef
sarttra42 <sarttra42@gmail.com>
2025-02-04 16:35:07 +0700

    'HiTue4'

--- my-app/src/components/Hellocomponent.js
+++
@@ -1,5 +0,0 @@
-function Hellocomponent(){
-    return <h1>สวัสดีครับ Component External</h1>
-}
-
-export default Hellocomponent
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,28 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Hellocomponent from './components/Hellocomponent';
+// import Hellocomponent from './components/Hellocomponent';
 
-// //การสร้าง Component functional
-// function Hellocomponent(){
-//   return <h1>สวัสดีครับ Component</h1>
-// }
-
-// //การสร้าง Component Class
-// class Hellocomponent extends React.Component{
-//   render() {
-//     return (
-//       <div>
-//         สวัสดีครับ
-//       </div>
-//     )
-//   }
-// }
-
-ReactDOM.render(<Hellocomponent/>,document.getElementById('root')
-);
+ReactDOM.render(<App/>,document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
